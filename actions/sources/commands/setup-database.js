35471a5d167b9877b682b37fe50cb07db30f6a19
Layne Faler <laynefaler@gmail.com>
2018-08-09 15:09:16 -0700

    Updates to templates

---
+++ actions/sources/commands/setup-database.js
@@ -0,0 +1,15 @@
+const description = 'Setup a SQL database for yourself';
+
+const command = (databaseType, options) => {
+    console.log('');
+};
+
+const documentation = () => {
+    console.info(`ENTER YOUR DOCUMENTATION`);
+};
+
+module.exports = {
+    command,
+    description,
+    documentation,
+};
