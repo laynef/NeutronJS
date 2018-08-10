bfda4f3f22e7208077da96c6922cb643d86c42d3
Layne Faler <laynefaler@gmail.com>
2018-08-10 11:04:58 -0700

    Updates to templates

---
+++ templates/newProject/controllers/v1/example/get.js
@@ -0,0 +1,24 @@
+module.exports = (req, res, docs=false) => {
+
+    /*
+
+    // To Import Controllers in Routes
+
+    // const C = require('./controllers');
+    // { controller: C.v1.example.get } // => Your controller function
+
+    Example:
+
+    User.findAll()
+    .then((response) => {
+        if (docs) return response || {};
+        else res.status(200).send(response);
+    })
+    .catch((error) => {
+        res.status(400).send({ error })
+    })
+
+
+    */
+
+};
