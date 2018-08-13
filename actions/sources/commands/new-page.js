427a12fabbf2df95669a00b06d886c5e0c8253d2
Layne Faler <laynefaler@gmail.com>
2018-08-13 10:48:11 -0700

    Added new route path to pug render function

--- actions/sources/commands/new-page.js
+++ actions/sources/commands/new-page.js
@@ -8,7 +8,7 @@ const description = 'Generate a new page with it\'s assets';
 const command = (pageName, routePath, options) => {
 
     if (!pageName || !routePath || !options) {
-        console.error('Must enter a page name');
+        console.error('Must enter a page name and it\'s route path');
         return;
     }
 
@@ -19,13 +19,11 @@ const command = (pageName, routePath, options) => {
     const root = process.cwd();
     const settings = require(path.join(root, 'webpack', 'settings.json'));
     const application = fs.readFileSync(path.join(root, 'app.js'), { encoding: 'utf8' });
-    shell.mkdir('-p', `${path.resolve(root, 'views','pages', routePath)}`);
-    fs.writeFileSync(path.resolve(root, 'views', 'pages', routePath, `${pageName}.pug`), newTemplate);
-    shell.mkdir('-p',`${path.resolve(root, 'assets', settings.styleType, 'pages', routePath)}`);
-    shell.mkdir('-p',`${path.resolve(root, 'assets', settings.jsType, 'pages', routePath)}`);
-    shell.cp(path.join(templatePath, 'assets', `page.${settings.styleType}`), path.resolve(root, 'assets', settings.styleType, 'pages', routePath, `${pageName}.${settings.styleType}`));
-    shell.cp(path.join(templatePath, 'assets', `page.${settings.jsType}`), path.resolve(root, 'assets', settings.jsType, 'pages', routePath, `${pageName}.${settings.jsType}`));
-    if (routePath) fs.writeFileSync(path.join(root, 'app.js'), application.replace(/\/\/ Leave Here For Static Routes/g, `// Leave Here For Static Routes\napp.get('${routePath}', render('${pageName}'));`));
+    shell.exec(`mkdir -p ${path.join(root, 'views','pages', routePath)} ${path.join(root, 'assets', settings.styleType, 'pages', routePath)} ${path.join(root, 'assets', settings.jsType, 'pages', routePath)}`);
+    fs.writeFileSync(path.join(root, 'views', 'pages', routePath, `${pageName}.pug`), newTemplate);
+    shell.cp(path.join(templatePath, 'assets', `page.${settings.styleType}`), path.join(root, 'assets', settings.styleType, 'pages', routePath, `${pageName}.${settings.styleType}`));
+    shell.cp(path.join(templatePath, 'assets', `page.${settings.jsType}`), path.join(root, 'assets', settings.jsType, 'pages', routePath, `${pageName}.${settings.jsType}`));
+    if (routePath) fs.writeFileSync(path.join(root, 'app.js'), application.replace(/\/\/ Leave Here For Static Routes/g, `// Leave Here For Static Routes\napp.get('${routePath}', render('pages/${routePath}/${pageName}'));`));
     console.green('Your new page assets have be created.')
 };
 
