d39b6a595499f7aa5386cfa1fc3e472e9f75bc81
Layne Faler <laynefaler@gmail.com>
2018-08-13 19:41:31 -0700

    Fixes to routes for automatic api docs with middleware descriptions

--- templates/newProject/routes/v1/index.js
+++ templates/newProject/routes/v1/index.js
@@ -11,7 +11,7 @@ const apiVersion = versionArray[versionArray.length - 1];
 
 const setRoutes = (array, method) => {
     array.forEach(e => {
-        let middleware = Array.isArray(e.middleware) && e.middleware.length > 0 ? e.middleware.map(e => Object.values(e.middleware)[0]) : [(req, res, next) => { next(); }];
+        let middleware = Array.isArray(e.middleware) && e.middleware.length > 0 ? e.middleware.map(e => Object.values(e)[0]) : [(req, res, next) => { next(); }];
         Promise.all([e.controller])
             .then((controller) => {
                 router[method](e.route, ...middleware, (req, res) => controller[0](req, res));
